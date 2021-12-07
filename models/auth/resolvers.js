abc2e479c4df5f71a3e38506a32fb3c6fe0ef5f8
josemua <chepemual@gmail.com>
2021-12-06 21:37:09 -0500

    control de errores de contraseña o usuario incorrecto en login

--- models/auth/resolvers.js
+++ models/auth/resolvers.js
@@ -7,50 +7,60 @@ const resolversAutenticacion = {
     registro: async (parent, args) => {
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(args.password, salt);
-      const usuarioCreado = await UserModel.create({
-        nombre: args.nombre,
-        apellido: args.apellido,
-        identificacion: args.identificacion,
-        correo: args.correo,
-        rol: args.rol,
-        password: hashedPassword,
-      });
-      return {
-        token: generateToken({
-          _id: usuarioCreado._id,
-          nombre: usuarioCreado.nombre,
-          apellido: usuarioCreado.apellido,
-          identificacion: usuarioCreado.identificacion,
-          correo: usuarioCreado.correo,
-          rol: usuarioCreado.rol,
-          estado: usuarioCreado.estado
-        }),
-      };
+      try {
+        const usuarioCreado = await UserModel.create({
+          nombre: args.nombre,
+          apellido: args.apellido,
+          identificacion: args.identificacion,
+          correo: args.correo,
+          rol: args.rol,
+          password: hashedPassword,
+        });
+        return {
+          token: generateToken({
+            _id: usuarioCreado._id,
+            nombre: usuarioCreado.nombre,
+            apellido: usuarioCreado.apellido,
+            identificacion: usuarioCreado.identificacion,
+            correo: usuarioCreado.correo,
+            rol: usuarioCreado.rol,
+            estado: usuarioCreado.estado,
+          }),
+        };
+      } catch (error) {
+        return { error: "El correo ya está registrado en la base de datos" };
+      }
     },
 
     login: async (parent, args) => {
       const usuarioEncontrado = await UserModel.findOne({
         correo: args.correo,
       });
-      if (await bcrypt.compare(args.password, usuarioEncontrado.password)) {
-        return {
-          token: generateToken({
-            _id: usuarioEncontrado._id,
-            nombre: usuarioEncontrado.nombre,
-            apellido: usuarioEncontrado.apellido,
-            identificacion: usuarioEncontrado.identificacion,
-            correo: usuarioEncontrado.correo,
-            rol: usuarioEncontrado.rol,
-            estado: usuarioEncontrado.estado
-          }),
-        };
+      try {
+        if (await bcrypt.compare(args.password, usuarioEncontrado.password)) {
+          return {
+            token: generateToken({
+              _id: usuarioEncontrado._id,
+              nombre: usuarioEncontrado.nombre,
+              apellido: usuarioEncontrado.apellido,
+              identificacion: usuarioEncontrado.identificacion,
+              correo: usuarioEncontrado.correo,
+              rol: usuarioEncontrado.rol,
+              estado: usuarioEncontrado.estado,
+            }),
+          };
+        } else {
+          return { error: "Contraseña equivocada" };
+        }
+      } catch (error) {
+        return { error: "Usuario no encontrado" };
       }
     },
 
     refreshToken: async (parent, args, context) => {
       if (!context.userData) {
         return {
-          error: 'token inválido',
+          error: "token inválido",
         };
       } else {
         return {
@@ -61,14 +71,12 @@ const resolversAutenticacion = {
             identificacion: context.userData.identificacion,
             correo: context.userData.correo,
             rol: context.userData.rol,
-            estado: context.userData.estado
+            estado: context.userData.estado,
           }),
         };
       }
     },
-
   },
-
 };
 
 export { resolversAutenticacion };
